261a5e065fd6a261d9160611cbc64d9d1a1ca61f
Laurence Armstrong <laurence.armstrong@hennge.com>
2019-12-13 16:04:04 +0900

    Start P2

---
+++ 10/p2/main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"sort"
+	"time"
+
+	day10 "github.com/LaurenceGA/adventofcode2019/10"
+)
+
+const (
+	asteroid = '#'
+	empty    = '.'
+)
+
+type coord struct {
+	X, Y int
+}
+
+var station = coord{
+	X: 22,
+	Y: 19,
+}
+
+type ByAngle []coord
+
+func (c ByAngle) Len() int           { return len(c) }
+func (c ByAngle) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c ByAngle) Less(i, j int) bool { return angleToStation(c[i]) < angleToStation(c[j]) }
+
+func angleToStation(c coord) float64 {
+	straightUpPoint := coord{X: station.X, Y: station.Y - 1}
+	p12 := distance(station, straightUpPoint)
+	p13 := distance(station, c)
+	p23 := distance(straightUpPoint, c)
+	angle := math.Acos((math.Pow(p12, 2) + math.Pow(p13, 2) - math.Pow(p23, 2)) / (2 * p12 * p13))
+	return angle
+}
+
+func main() {
+	start := time.Now()
+
+	asteroidMap := day10.GetInput()
+	detectable := detectableAsteroids(asteroidMap, station)
+	sort.Sort(ByAngle(detectable))
+
+	fmt.Println(detectable)
+	goalCoord := detectable[200]
+	fmt.Println(goalCoord)
+	fmt.Println(goalCoord.X*100 + goalCoord.Y)
+
+	fmt.Println("Time elapsed:", time.Since(start))
+}
+
+func detectableAsteroids(asteroidMap [][]rune, pos coord) []coord {
+	//printMap(asteroidMap)
+	others := otherAsteroids(asteroidMap, pos)
+	var detectableAsteroids []coord
+
+	for _, o := range others {
+		if canSee(pos, o, asteroidMap) {
+			detectableAsteroids = append(detectableAsteroids, o)
+		}
+	}
+
+	//fmt.Println()
+	//printMap(generateDetectionMap(asteroidMap, detectableAsteroids, pos))
+
+	return detectableAsteroids
+}
+
+func canSee(a, b coord, asteroidMap [][]rune) bool {
+	between := coordsBetween(a, b)
+	for _, possibleInterceptor := range between {
+		if possibleInterceptor != b && asteroidMap[possibleInterceptor.Y][possibleInterceptor.X] == asteroid {
+			return false
+		}
+	}
+
+	return true
+}
+
+func coordsBetween(a, b coord) []coord {
+	if a == b {
+		return []coord{}
+	}
+
+	var intermediateCoords []coord
+	xDir, yDir := 1, 1
+	if b.X < a.X {
+		xDir = -1
+	}
+	if b.Y < a.Y {
+		yDir = -1
+	}
+	for i := 0; i < int(math.Abs(float64(b.X-a.X)))+1; i++ {
+		xCoord := a.X + i*xDir
+		for j := 0; j < int(math.Abs(float64(b.Y-a.Y)))+1; j++ {
+			yCoord := a.Y + j*yDir
+			if (xCoord == a.X && yCoord == a.Y) || (xCoord == b.X && yCoord == b.Y) {
+				continue
+			}
+
+			curCoord := coord{X: xCoord, Y: yCoord}
+			if liesBetween(a, b, curCoord) {
+				intermediateCoords = append(intermediateCoords, curCoord)
+			}
+		}
+	}
+
+	return intermediateCoords
+}
+
+// Does c lie on the line between a and b?
+func liesBetween(a, b, c coord) bool {
+	threshold := 0.000001
+	distSumDiff := (distance(a, c) + distance(c, b)) - distance(a, b)
+	return -threshold < distSumDiff && distSumDiff < threshold
+}
+
+func distance(a, b coord) float64 {
+	return math.Sqrt(math.Pow(float64(a.X-b.X), 2) + math.Pow(float64(a.Y-b.Y), 2))
+}
+
+func getLine(a, b coord) (slope, intercept float64, err error) {
+	if (b.X - a.X) == 0 {
+		err = errors.New("div by 0")
+		return
+	}
+	slope = float64(b.Y-a.Y) / float64(b.X-a.X)
+	intercept = float64(a.Y) - slope*float64(a.X)
+	return
+}
+
+func otherAsteroids(asteroidMap [][]rune, pos coord) []coord {
+	var asteroids []coord
+	for y, row := range asteroidMap {
+		for x, ast := range row {
+			if ast == asteroid && !(x == pos.X && y == pos.Y) {
+				asteroids = append(asteroids, coord{X: x, Y: y})
+			}
+		}
+	}
+
+	return asteroids
+}
+
+func printMap(asteroidMap [][]rune) {
+	for _, row := range asteroidMap {
+		for _, ast := range row {
+			fmt.Print(string(ast))
+		}
+		fmt.Println()
+	}
+}
+
+func generateDetectionMap(originalMap [][]rune, detectables []coord, pos coord) [][]rune {
+	newMap := make([][]rune, len(originalMap))
+	for i := range newMap {
+		newMap[i] = make([]rune, len(originalMap[i]))
+		for j := range newMap[i] {
+			newMap[i][j] = empty
+		}
+	}
+	for _, obj := range detectables {
+		newMap[obj.Y][obj.X] = asteroid
+	}
+
+	newMap[pos.Y][pos.X] = 'X'
+
+	return newMap
+}
---
+++ 10/p2/main_test.go
@@ -0,0 +1,265 @@
+package main
+
+import (
+	"strconv"
+	"testing"
+
+	day10 "github.com/LaurenceGA/adventofcode2019/10"
+)
+
+const (
+	testMap1 = `.#..#
+.....
+#####
+....#
+...##`
+
+	testMap2 = `......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####`
+	testMap3 = `#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###.`
+	testMap4 = `.#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#..`
+	testMap5 = `.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##`
+)
+
+func TestAsteroidsDetectable(t *testing.T) {
+	for _, tt := range []struct {
+		name                string
+		asteroidMapInput    string
+		pos                 coord
+		detectableAsteroids int
+	}{
+		{
+			name:                "1, (1, 0)",
+			asteroidMapInput:    testMap1,
+			pos:                 coord{X: 1, Y: 0},
+			detectableAsteroids: 7,
+		},
+		{
+			name:                "1, (4, 0)",
+			asteroidMapInput:    testMap1,
+			pos:                 coord{X: 4, Y: 0},
+			detectableAsteroids: 7,
+		},
+		{
+			name:                "1, (0, 2)",
+			asteroidMapInput:    testMap1,
+			pos:                 coord{X: 0, Y: 2},
+			detectableAsteroids: 6,
+		},
+		{
+			name:                "1, (1, 2)",
+			asteroidMapInput:    testMap1,
+			pos:                 coord{X: 1, Y: 2},
+			detectableAsteroids: 7,
+		},
+		{
+			name:                "1, (2, 2)",
+			asteroidMapInput:    testMap1,
+			pos:                 coord{X: 2, Y: 2},
+			detectableAsteroids: 7,
+		},
+		{
+			name:                "1, (3, 2)",
+			asteroidMapInput:    testMap1,
+			pos:                 coord{X: 3, Y: 2},
+			detectableAsteroids: 7,
+		},
+		{
+			name:                "1, (4, 2)",
+			asteroidMapInput:    testMap1,
+			pos:                 coord{X: 4, Y: 2},
+			detectableAsteroids: 5,
+		},
+		{
+			name:                "1, (4, 3)",
+			asteroidMapInput:    testMap1,
+			pos:                 coord{X: 4, Y: 3},
+			detectableAsteroids: 7,
+		},
+		{
+			name:                "1, (3, 4)",
+			asteroidMapInput:    testMap1,
+			pos:                 coord{X: 3, Y: 4},
+			detectableAsteroids: 8,
+		},
+		{
+			name:                "1, (4, 4)",
+			asteroidMapInput:    testMap1,
+			pos:                 coord{X: 4, Y: 4},
+			detectableAsteroids: 7,
+		},
+		{
+			name:                "2, (5, 8)",
+			asteroidMapInput:    testMap2,
+			pos:                 coord{X: 5, Y: 8},
+			detectableAsteroids: 33,
+		},
+		{
+			name:                "3, (1, 2)",
+			asteroidMapInput:    testMap3,
+			pos:                 coord{X: 1, Y: 2},
+			detectableAsteroids: 35,
+		},
+		{
+			name:                "4, (6, 3)",
+			asteroidMapInput:    testMap4,
+			pos:                 coord{X: 6, Y: 3},
+			detectableAsteroids: 41,
+		},
+		{
+			name:                "5, (11, 13)",
+			asteroidMapInput:    testMap5,
+			pos:                 coord{X: 11, Y: 13},
+			detectableAsteroids: 210,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			aMap := day10.ProcessInput(tt.asteroidMapInput)
+			numDetectable := len(detectableAsteroids(aMap, tt.pos))
+
+			if numDetectable != tt.detectableAsteroids {
+				t.Errorf("Expected %d detectable asteroids, found %d", tt.detectableAsteroids, numDetectable)
+			}
+		})
+
+	}
+}
+
+func TestGetLine(t *testing.T) {
+	for _, tt := range []struct {
+		a, b             coord
+		err              bool
+		slope, intercept float64
+	}{
+		{
+			a:         coord{X: 0, Y: 0},
+			b:         coord{X: 5, Y: 0},
+			slope:     0,
+			intercept: 0,
+		},
+		{
+			a:         coord{X: 3, Y: 7},
+			b:         coord{X: 5, Y: 11},
+			slope:     2,
+			intercept: 1,
+		},
+		{
+			a:   coord{X: 4, Y: 0},
+			b:   coord{X: 4, Y: 3},
+			err: true,
+		},
+	} {
+		slope, intercept, err := getLine(tt.a, tt.b)
+		if slope != tt.slope {
+			t.Errorf("Expected slope %f, got %f", tt.slope, slope)
+		}
+		if intercept != tt.intercept {
+			t.Errorf("Expected intercept %f, got %f", tt.intercept, intercept)
+		}
+		if (err != nil) != tt.err {
+			t.Errorf("Expected err %v, got %v", tt.err, err != nil)
+		}
+	}
+}
+
+func TestLiesBetween(t *testing.T) {
+	for i, tt := range []struct {
+		a, b, c  coord
+		expected bool
+	}{
+		{
+			a:        coord{X: 0, Y: 0},
+			b:        coord{X: 5, Y: 0},
+			c:        coord{X: 3, Y: 0},
+			expected: true,
+		},
+		{
+			a:        coord{X: 0, Y: 0},
+			b:        coord{X: 5, Y: 0},
+			c:        coord{X: 3, Y: 1},
+			expected: false,
+		},
+		{
+			a:        coord{X: 3, Y: 4},
+			b:        coord{X: 1, Y: 0},
+			c:        coord{X: 2, Y: 2},
+			expected: true,
+		},
+		{
+			a:        coord{X: 0, Y: 0},
+			b:        coord{X: 1, Y: 1},
+			c:        coord{X: 2, Y: 2},
+			expected: false,
+		},
+		{
+			a:        coord{X: 1, Y: 0},
+			b:        coord{X: 1, Y: 1},
+			c:        coord{X: 1, Y: 2},
+			expected: false,
+		},
+		{
+			a:        coord{X: 1, Y: 1},
+			b:        coord{X: 2, Y: 1},
+			c:        coord{X: 3, Y: 1},
+			expected: false,
+		},
+		{
+			a:        coord{X: 1, Y: 0},
+			b:        coord{X: 4, Y: 3},
+			c:        coord{X: 3, Y: 2},
+			expected: true,
+		},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			between := liesBetween(tt.a, tt.b, tt.c)
+			if between != tt.expected {
+				t.Errorf("Expected between=%v, got %v", tt.expected, between)
+			}
+		})
+	}
+}
